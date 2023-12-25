71d9ae98013bacbede604cf1e41c8aec90b503b7
yousefsalahkassem <yousef.salah12345678@gmail.com>
2023-12-25 10:27:55 +0200

    add action if exist

--- lib/src/widgets/cart/add_to_cart_button.dart
+++ lib/src/widgets/cart/add_to_cart_button.dart
@@ -2,20 +2,28 @@ import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import '../../../fancy_cart.dart';
 
-
 class AddToCartButton extends ConsumerWidget {
   final Widget child;
   final Function? actionAfterAdding;
+  final Function? actionIfExist;
   final CartItem cartModel;
-  const AddToCartButton({Key? key, required this.child, this.actionAfterAdding,required this.cartModel})
-      : super(key: key);
+
+  const AddToCartButton({
+    Key? key,
+    required this.child,
+    this.actionAfterAdding,
+    this.actionIfExist,
+    required this.cartModel,
+  }) : super(key: key);
 
   @override
-  Widget build(BuildContext context,WidgetRef ref) {
+  Widget build(BuildContext context, WidgetRef ref) {
     return GestureDetector(
-      onTap: (){
-        ref.read(CartNotifier.provider.notifier).addItem(cartModel);
-        if(actionAfterAdding != null){
+      onTap: () {
+        ref
+            .read(CartNotifier.provider.notifier)
+            .addItem(cartModel, actionIfExist: actionIfExist);
+        if (actionAfterAdding != null) {
           actionAfterAdding!();
         }
       },
